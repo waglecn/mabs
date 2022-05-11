f28c3360255cce0ce68e4cff30f2f5e0b10b87a0
Nick Waglechner <NWaglechner@gmail.com>
2022-05-11 11:21:09 -0400

    updates smart read merging script

--- scripts/smart_merge.py
+++ scripts/smart_merge.py
@@ -2,16 +2,21 @@
 
 import sys
 import os
+from pathlib import Path
 import subprocess
 
 infiles = sys.argv[2:]
 outfile = sys.argv[1]
+tempout = Path(outfile).stem
 
 if len(infiles) > 1:
-    cmd = ['zcat'] + infiles
-    with open(outfile, 'w') as outf:
-        subprocess.run(cmd, stdout=outf)
-    print("Merged eith {}".format(cmd, outfile))
+    cmd1 = ['zcat'] + infiles
+    with open(tempout, 'w') as outf:
+        subprocess.run(cmd1, stdout=outf)
+    cmd2 = ['gzip', tempout]
+    subprocess.run(cmd2)
+    os.rename(tempout, outfile)
+    print("Merged: {}".format(cmd2, outfile))
 elif len(infiles) == 1:
     os.symlink(infiles[0], outfile)
     print("Created link {} -> {}".format(infiles[0], outfile))
