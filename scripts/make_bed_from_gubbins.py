64642879b180ce983acbf307da4c16e9d8c94556
Nick Waglechner <NWaglechner@gmail.com>
2022-05-11 11:22:14 -0400

    fixes make bed from gubbins script

--- scripts/make_bed_from_gubbins.py
+++ scripts/make_bed_from_gubbins.py
@@ -8,46 +8,31 @@ from Bio import SeqIO
 
 def main():
     # input gubbins recombination embl
-    sentinel = sys.argv[1]
-    ref_file = os.path.split(sentinel)[-1].split('.')[0]
-    
-    if len(sys.argv) > 2:
-        id = sys.argv[2]
-    else:
-        id = 'NC_010397.1'
-        base_file = pathlib.Path(sentinel).resolve().parent.parent.parent / 'resources' / \
-            'alignment_references' / f"{ref_file}.fasta"
-        # base_file = os.path.join(
-        #    '..',  
-        # )
-        records = [r for r in SeqIO.parse(str(base_file), 'fasta')]
-        id = records[0].id
-        print(
-            'Found: {} -- genome accession to use for bed: {}'.format(
-                base_file, id
-            ), file=sys.stderr
-        )
-
-
-    infile = os.path.join(
-        os.path.split(sentinel)[0],
-        ref_file + '.concatenated.recombination_predictions.embl'
+    embl = sys.argv[1]
+    ref_file = sys.argv[2]
+    records = [r for r in SeqIO.parse(str(ref_file), 'fasta')]
+    id = records[0].id
+    print(
+        'Found: {} -- genome accession to use for bed: {}'.format(
+            ref_file, id
+        ),
+        file=sys.stderr
     )
-    
 
     try:
-        lines = open(infile, 'r').readlines()
+        lines = open(embl, 'r').readlines()
         lines = [l.strip() for l in lines if 'misc_feature' in l]
         lines =[l.split(' ')[-1].split('.') for l in lines]
-        print("Found: {}".format(infile), file=sys.stderr)
+        print("Found: {}".format(embl), file=sys.stderr)
     except FileNotFoundError:
         print("Not Found: {} -- blank bed".format(infile), file=sys.stderr)
         lines = []
-    
+
     for l in lines:
         print('{}\t{}\t{}\tgubbins'.format(
             id, l[0], l[-1]
         ))
+
 
 if __name__ == '__main__':
     main()
